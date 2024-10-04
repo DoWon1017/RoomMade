c6594fa20db85958f9559db6c6bd3bb2a08a5b9c
Drag0 <tomger1017@gmail.com>
2024-10-04 23:22:07 +0900

    내가 쓴글 수정

--- app/src/main/java/com/example/roommade/FreeBoardPost.java
+++ app/src/main/java/com/example/roommade/FreeBoardPost.java
@@ -7,6 +7,11 @@ public class FreeBoardPost {
     private long timestamp;
     private String postId;
 
+    // 기본 생성자 (매개변수 없음)
+    public FreeBoardPost() {
+        // Firestore 역직렬화에 필요
+    }
+
     public FreeBoardPost(String title, String content, String userId, long timestamp, String postId) {
         this.title = title;
         this.content = content;
